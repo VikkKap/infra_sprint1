64dbaec701e5ac545844675a4f675c898f28a703
VikkKap <124706417+VikkKap@users.noreply.github.com>
2023-09-09 21:56:17 +0500

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG', default='False')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['51.250.21.57', '127.0.0.1',
                  'localhost', 'kittygram-vik.ddns.net']
